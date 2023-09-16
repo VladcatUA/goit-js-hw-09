10b3105278b7c12acf5da7a9f21375b1663c6b6e
Vladyslav <vladislav2002vladislav2002@gmail.com>
2023-09-16 18:45:38 +0300

    task1and2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const colorBody = document.querySelector('body');
+let timerId;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+btnStart.addEventListener('click', () => {
+    const initialColor = getRandomHexColor();
+    colorBody.style.backgroundColor = initialColor;
+
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        colorBody.style.backgroundColor = randomColor;
+  }, 1000);
+  btnStart.setAttribute("disabled", true);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStart.removeAttribute('disabled');
+});
